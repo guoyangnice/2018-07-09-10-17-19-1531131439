08a112516ce1072224404c4361d3cd5ec0beb6f6
guoyangnice <yangjienice@126.com>
2019-08-22 22:11:19 +0800

    6 pass

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Student {
+import practice05.Person;
+
+public class Student extends Person{
+
+	private int klass;
+	
+	public Student(String name, int age,int klass) {
+		super(name, age);
+		this.klass = klass;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getKlass() {
+		return klass;
+	}
+	
+	
+	public String introduce() {
+		super.introduce();
+		System.out.println(super.introduce()+" I am a Student. I am at Class "+ getKlass() + ".");
+		return super.introduce()+" I am a Student. I am at Class "+ getKlass() + ".";
+	}
+	
 }
