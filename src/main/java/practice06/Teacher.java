517d8174f133a95ff0795807700a9acd6f3cd6c3
guoyangnice <yangjienice@126.com>
2019-08-22 23:01:28 +0800

    commit
    6 pass

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+	
+	private int klass;
+	
+	public Teacher(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Teacher(String name, int age,int klass) {
+		super(name, age);
+		this.klass = klass;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getKlass() {
+		return klass;
+	}
+	
+	public String introduce() {
+		if(klass == 2) {
+			return super.introduce() + " I am a Teacher. I teach Class " +getKlass()+ ".";
+		}
+		return super.introduce() + " I am a Teacher. I teach " + "No Class.";
+		
+	}
+	
 }
